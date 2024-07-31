86118c6281dc5f17b1c0e9ca5c86793805076445
jacoEafit <jmontesca@eafit.edu.co>
2024-07-31 11:03:51 -0500

    segundo commit

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'home.html',{'name':'Jacobo Montes'})
+
+
+def about(request):
+    return render(request,'about.html',{'name':'Jacobo Montes'})
